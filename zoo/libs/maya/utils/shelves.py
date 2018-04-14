2818a83ecd169b906514bc8502204f5b54a6bc0f
David Sparrow <dsparrow27@gmail.com>
2018-04-14 13:37:26 +1000

    shelves to support separators

--- zoo/libs/maya/utils/shelves.py
+++ zoo/libs/maya/utils/shelves.py
@@ -12,28 +12,40 @@ class Shelf(object):
         self.labelColour = (.9, .9, .9)
 
     def createShelf(self):
+        assert not cmds.shelfLayout(self.name, exists=True), "Shelf by the name {} already exists".format(self.name)
         self.name = cmds.shelfLayout(self.name, parent="ShelfLayout")
 
-    def addButton(self, label, icon="commandButton.png", command=None, doubleCommand=None, Type="python"):
+    def addButton(self, label, tooltip=None, icon="commandButton.png", command=None, doubleCommand=None, Type="python"):
         """Adds a shelf button with the specified label, command, double click command and image."""
+
         cmds.setParent(self.name)
-        icon = icon or ""
         command = command or ""
         doubleCommand = doubleCommand or ""
-        return cmds.shelfButton(width=37, height=37, image=icon, label=label, command=command,
-                                doubleClickCommand=doubleCommand,
+        return cmds.shelfButton(width=37, height=37, image=icon or "", label=label, command=command,
+                                doubleClickCommand=doubleCommand, annotation=tooltip or "",
                                 imageOverlayLabel=label, overlayLabelBackColor=self.labelBackground,
                                 overlayLabelColor=self.labelColour, sourceType=Type)
+    @staticmethod
+    def addMenuItem(parent, label, command="", icon=""):
+        """Adds a menu item with the specified label, command, and image."""
 
-    def addMenuItem(self, parent, label, command="", icon=""):
-        """Adds a shelf button with the specified label, command, and image."""
-        icon = icon or ""
-        return cmds.menuItem(parent=parent, label=label, command=command, icon=icon)
+        return cmds.menuItem(parent=parent, label=label, command=command, image=icon or "")
 
-    def addSubMenu(self, parent, label, icon=None):
+    @staticmethod
+    def addSubMenu(parent, label, icon=None):
         """Adds a sub menu item with the specified label and icon to the specified parent popup menu."""
-        icon = icon or ""
-        return cmds.menuItem(parent=parent, label=label, icon=icon, subMenu=1)
+        return cmds.menuItem(parent=parent, label=label, icon=icon or "", subMenu=1)
+
+    def addSeparator(self, parent):
+        """ Adds a maya shelf separator to the parent shelf
+
+        :param parent: The full path to the shelf
+        :type parent: str
+        :return: The full path to the separator
+        :rtype: str
+        """
+        cmds.separator(parent=self.name, manage=True, visible=True,
+                       horizontal=True, style="shelf", enableBackground=False, preventOverride=False)
 
     def _cleanOldShelf(self):
         """Checks if the shelf exists and empties it if it does or creates it if it does not."""
