3a69dda44096dc986cb90ef42720f0f44c77fe98
Keen <keenfoong@gmail.com>
2018-10-31 14:52:09 +1100

    testing to see if we can get docking working

--- zoo/libs/maya/qt/mayaui.py
+++ zoo/libs/maya/qt/mayaui.py
@@ -1,12 +1,15 @@
 from qt import QtWidgets, QtCore, QtGui
 
 try:
-    from shiboken2 import wrapInstance as wrapinstance
+    from shiboken2 import wrapInstance as wrapinstance, getCppPointer
 except:
     from shiboken import wrapInstance as wrapinstance
 from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
 import maya.OpenMayaUI as apiUI
+
 from maya import cmds
+
+mixinWorkspaceControls = dict()
 
 
 def dpiScale(value):
@@ -253,11 +256,12 @@ class BootStrapWidget(MayaQWidgetDockableMixin, QtWidgets.QWidget):
         kwargs["retain"] = False
         # create the ui script which launches the custom widget, autodesk decided that string are god eeekkkkk!
         kwargs["uiScript"] = "try: import zoo.libs.maya.qt.mayaui as zoomayaui;zoomayaui.rebuild({})\nexcept ImportError: pass".format(self.objectName())
+
         # create the close callback string autodesk wacky design decisions again
         kwargs["closeCallback"] = 'try: import zoo.libs.maya.qt.mayaui as zoomayaui;zoomayaui.bootstrapDestroyWindow("{}")\nexcept ImportError: pass'.format(self.objectName())
-        super(BootStrapWidget, self).show(**kwargs)
+        #super(BootStrapWidget, self).show(**kwargs)
 
-        #self.dockableShow(**kwargs)
+        self.dockableShow(**kwargs)
 
     def dockableShow(self, *args, **kwargs):
         """ Copied from mayaMixin.MayaQWidgetDockableMixin().show() so we can tweak the docking settings.
@@ -275,22 +279,284 @@ class BootStrapWidget(MayaQWidgetDockableMixin, QtWidgets.QWidget):
                            True)  # NOTE: Explicitly calling QWidget.setVisible() as using super() breaks in PySide: super(self.__class__, self).show()
 
         # Handle special case if the parent is a QDockWidget (dockControl)
+        print kwargs
         parent = self.parent()
         if parent:
             parentName = parent.objectName()
             if parentName and len(parentName) and cmds.workspaceControl(parentName, q=True, exists=True):
+                print (parentName)
                 if cmds.workspaceControl(parentName, q=True, visible=True):
+                    print ("visible")
                     cmds.workspaceControl(parentName, e=True, restore=True)
                 else:
+                    print ("invisible")
                     if kwargs['dockable']:
                         print ("dockable")
-                        cmds.workspaceControl(parentName, e=True, visible=True)
+                        script= """
+from qt import QtCore, QtWidgets, QtGui
+from shiboken2 import wrapInstance as wrapinstance
+workspaceControlPtr = apiUI.MQtUtil.getCurrentParent()
+mw = wrapinstance(long(workspaceControlPtr), QtWidgets.QMainWindow)
+mw.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+mw.setWindowFlags(mw.windowFlags() | QtCore.Qt.FramelessWindowHint)                        
+                        """
+                        cmds.workspaceControl(parentName, e=True, loadImmediately=True, uiScript=script, visible=True)
+
+
                     else:
-                        print("not dockable")
                         # Create our own so we can have our own transparent background
                         ptr = apiUI.MQtUtil.getCurrentParent()
                         mw = wrapinstance(long(ptr), QtWidgets.QMainWindow)
                         mw.show()
+
+    def setDockableParameters(self, dockable=None, floating=None, area=None, allowedArea=None, width=None,
+                              widthSizingProperty=None, minWidth=None, height=None, heightSizingProperty=None,
+                              x=None, y=None, retain=True, plugins=None, controls=None, uiScript=None,
+                              closeCallback=None, *args, **kwargs):
+        '''
+        Set the dockable parameters.
+
+        :Parameters:
+            dockable (bool)
+                Specify if the window is dockable (default=False)
+            floating (bool)
+                Should the window be floating or docked (default=True)
+            area (string)
+                Default area to dock into (default='left')
+                Options: 'top', 'left', 'right', 'bottom'
+            allowedArea (string)
+                Allowed dock areas (default='all')
+                Options: 'top', 'left', 'right', 'bottom', 'all'
+            width (int)
+                Width of the window
+            height (int)
+                Height of the window
+            x (int)
+                left edge of the window
+            y (int)
+                top edge of the window
+
+        :See: show(), hide(), and setVisible()
+        '''
+        print "Dockable show"
+        if ((dockable == True) or (dockable is None and self.isDockable())):  # == Handle docked window ==
+            # By default, when making dockable, make it floating
+            #   This addresses an issue on Windows with the window decorators not showing up.
+            if floating is None and area is None:
+                floating = True
+
+            # Create workspaceControl if needed
+            if dockable == True and not self.isDockable():
+                # Retrieve original position and size
+                # Position
+                if x is None:
+                    x = self.x()
+                    # Give suitable default value if null
+                    if x == 0:
+                        x = 250
+                if y is None:
+                    y = self.y()
+                    # Give suitable default value if null
+                    if y == 0:
+                        y = 200
+                # Size
+                unininitializedSize = QtCore.QSize(640, 480)  # Hardcode: (640,480) is the default size for a QWidget
+                if self.size() == unininitializedSize:
+                    # Get size from widget sizeHint if size not yet initialized (before the first show())
+                    widgetSizeHint = self.sizeHint()
+                else:
+                    widgetSizeHint = self.size()  # use the current size of the widget
+                if width is None:
+                    width = widgetSizeHint.width()
+                if height is None:
+                    height = widgetSizeHint.height()
+                if widthSizingProperty is None:
+                    widthSizingProperty = 'free'
+                if heightSizingProperty is None:
+                    heightSizingProperty = 'free'
+
+                if controls is None:
+                    controls = []
+                if plugins is None:
+                    plugins = []
+
+                workspaceControlName = self.objectName() + 'WorkspaceControl'
+                # Set to floating if requested or if no docking area given
+                if floating == True or area is None:
+                    print ("begin")
+                    if minWidth is None:
+                        script = """
+print( "dockable show")
+from qt import QtCore, QtWidgets, QtGui
+from shiboken2 import wrapInstance as wrapinstance
+workspaceControlPtr = apiUI.MQtUtil.getCurrentParent()
+mw = wrapinstance(long(workspaceControlPtr), QtWidgets.QMainWindow)
+mw.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+mw.setWindowFlags(mw.windowFlags() | QtCore.Qt.FramelessWindowHint)"""
+
+                        workspaceControlName = cmds.workspaceControl(workspaceControlName, label=self.windowTitle(),
+                                                                     retain=retain, loadImmediately=True,
+                                                                     floating=True, initialWidth=width,
+                                                                     widthProperty=widthSizingProperty,
+                                                                     initialHeight=height,
+                                                                     heightProperty=heightSizingProperty,
+                                                                     requiredPlugin=plugins,
+                                                                     requiredControl=controls,
+                                                                     uiScript=script,
+                                                                     vis=False)
+
+
+
+                    else:
+                        workspaceControlName = cmds.workspaceControl(workspaceControlName, label=self.windowTitle(),
+                                                                     retain=retain, loadImmediately=True,
+                                                                     floating=True, initialWidth=width,
+                                                                     widthProperty=widthSizingProperty,
+                                                                     minimumWidth=minWidth, initialHeight=height,
+                                                                     heightProperty=heightSizingProperty,
+                                                                     requiredPlugin=plugins,
+                                                                     requiredControl=controls)
+
+                else:
+                    if self.parent() is None or (
+                        long(getCppPointer(self.parent())[0]) == long(apiUI.MQtUtil.mainWindow())):
+                        # If parented to the Maya main window or nothing, dock into the Maya main window
+                        if minWidth is None:
+                            workspaceControlName = cmds.workspaceControl(workspaceControlName,
+                                                                         label=self.windowTitle(), retain=retain,
+                                                                         loadImmediately=True,
+                                                                         dockToMainWindow=(area, False),
+                                                                         initialWidth=width,
+                                                                         widthProperty=widthSizingProperty,
+                                                                         initialHeight=height,
+                                                                         heightProperty=heightSizingProperty,
+                                                                         requiredPlugin=plugins,
+                                                                         requiredControl=controls)
+                            print ("5")
+                        else:
+                            workspaceControlName = cmds.workspaceControl(workspaceControlName,
+                                                                         label=self.windowTitle(), retain=retain,
+                                                                         loadImmediately=True,
+                                                                         dockToMainWindow=(area, False),
+                                                                         initialWidth=width,
+                                                                         widthProperty=widthSizingProperty,
+                                                                         minimumWidth=minWidth,
+                                                                         initialHeight=height,
+                                                                         heightProperty=heightSizingProperty,
+                                                                         requiredPlugin=plugins,
+                                                                         requiredControl=controls)
+                    else:
+                        # Otherwise, the parent should be within a workspace control - need to go up the hierarchy to find it
+                        foundParentWorkspaceControl = False
+                        nextParent = self.parent()
+                        while nextParent is not None:
+                            dockToWorkspaceControlName = nextParent.objectName()
+                            if cmds.workspaceControl(dockToWorkspaceControlName, q=True, exists=True):
+                                if minWidth is None:
+                                    workspaceControlName = cmds.workspaceControl(workspaceControlName,
+                                                                                 label=self.windowTitle(),
+                                                                                 retain=retain,
+                                                                                 loadImmediately=True,
+                                                                                 dockToControl=(
+                                                                                 dockToWorkspaceControlName, area),
+                                                                                 initialWidth=width,
+                                                                                 widthProperty=widthSizingProperty,
+                                                                                 initialHeight=height,
+                                                                                 heightProperty=heightSizingProperty,
+                                                                                 requiredPlugin=plugins,
+                                                                                 requiredControl=controls)
+                                else:
+                                    workspaceControlName = cmds.workspaceControl(workspaceControlName,
+                                                                                 label=self.windowTitle(),
+                                                                                 retain=retain,
+                                                                                 loadImmediately=True,
+                                                                                 dockToControl=(
+                                                                                 dockToWorkspaceControlName, area),
+                                                                                 initialWidth=width,
+                                                                                 widthProperty=widthSizingProperty,
+                                                                                 minimumWidth=minWidth,
+                                                                                 initialHeight=height,
+                                                                                 heightProperty=heightSizingProperty,
+                                                                                 requiredPlugin=plugins,
+                                                                                 requiredControl=controls)
+                                foundParentWorkspaceControl = True
+                                break
+                            else:
+                                nextParent = nextParent.parent()
+
+                        if foundParentWorkspaceControl == False:
+                            # If parent workspace control cannot be found, just make the workspace control a floating window
+                            if minWidth is None:
+                                workspaceControlName = cmds.workspaceControl(workspaceControlName,
+                                                                             label=self.windowTitle(),
+                                                                             retain=retain, loadImmediately=True,
+                                                                             floating=True, initialWidth=width,
+                                                                             widthProperty=widthSizingProperty,
+                                                                             initialHeight=height,
+                                                                             heightProperty=heightSizingProperty,
+                                                                             requiredPlugin=plugins,
+                                                                             requiredControl=controls)
+                            else:
+                                workspaceControlName = cmds.workspaceControl(workspaceControlName,
+                                                                             label=self.windowTitle(),
+                                                                             retain=retain, loadImmediately=True,
+                                                                             floating=True, initialWidth=width,
+                                                                             widthProperty=widthSizingProperty,
+                                                                             minimumWidth=minWidth,
+                                                                             initialHeight=height,
+                                                                             heightProperty=heightSizingProperty,
+                                                                             requiredPlugin=plugins,
+                                                                             requiredControl=controls)
+
+                currParent = apiUI.MQtUtil.getCurrentParent()
+                mixinPtr = apiUI.MQtUtil.findControl(self.objectName())
+                apiUI.MQtUtil.addWidgetToMayaLayout(long(mixinPtr), long(currParent))
+
+                if uiScript is not None and len(uiScript):
+                    cmds.workspaceControl(workspaceControlName, e=True, uiScript=uiScript)
+
+                if closeCallback is not None:
+                    cmds.workspaceControl(workspaceControlName, e=True, closeCommand=closeCallback)
+
+                # Add this control to the list of controls created in Python
+                global mixinWorkspaceControls
+                mixinWorkspaceControls[workspaceControlName] = self
+
+                # Hook up signals
+                # dockWidget.topLevelChanged.connect(self.floatingChanged)
+                # dockWidget.closeEventTriggered.connect(self.dockCloseEventTriggered)
+
+        else:  # == Handle Standalone Window ==
+            # Make standalone as needed
+            if not dockable and self.isDockable():
+                # Retrieve original position and size
+                dockPos = self.parent().pos()
+                if x == None:
+                    x = dockPos.x()
+                if y == None:
+                    y = dockPos.y()
+                if width == None:
+                    width = self.width()
+                if height == None:
+                    height = self.height()
+                # Turn into a standalone window and reposition
+                currentVisibility = self.isVisible()
+                self._makeMayaStandaloneWindow()  # Set the parent back to Maya and remove the parent dock widget
+                self.setVisible(currentVisibility)
+
+            # Handle position and sizing
+            if (width != None) or (height != None):
+                if width == None:
+                    width = self.width()
+                if height == None:
+                    height = self.height()
+                self.resize(width, height)
+            if (x != None) or (y != None):
+                if x == None:
+                    x = self.x()
+                if y == None:
+                    y = self.y()
+                self.move(x, y)
 
 
 
